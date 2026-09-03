02b6d044adac5501057c4c82e9c271443dabdb3e
JxberV <jabervargas1726@gmail.com>
2026-09-03 01:41:10 -0500

    feat: registrar cuenta avanzado

--- 03-Development/Src/Frontend/Scripts/app.js
+++ 03-Development/Src/Frontend/Scripts/app.js
@@ -1,9 +1,65 @@
-const menuToggle = document.querySelector('.menu-toggle');
-const mainNav = document.querySelector('.main-nav');
 
-if (menuToggle && mainNav) {
-    menuToggle.addEventListener('click', () => {
-        const isOpen = mainNav.classList.toggle('open');
-        menuToggle.setAttribute('aria-expanded', String(isOpen));
+const botonMenu = document.querySelector('.menu-toggle');
+const navegacion = document.querySelector('.main-nav');
+
+//formularios
+const formularioRegistro = document.querySelector('.auth-form')
+const inputNombre = document.querySelector('#name');
+const inputEmailRegistro = document.querySelector('#register-email');
+const selectPerfil = document.querySelector('#role');
+const inputPasswordRegistro = document.querySelector('#register-password');
+const checkTerminos = document.querySelector('.check input[type="checkbox"]');
+
+if (botonMenu && navegacion) {
+
+
+    botonMenu.addEventListener('click', () => {
+
+
+        const menuEstaAbierto = navegacion.classList.contains('open');
+
+        if (menuEstaAbierto) {
+
+            navegacion.classList.remove('open');
+            botonMenu.setAttribute('aria-expanded', 'false');
+        } else {
+
+            navegacion.classList.add('open');
+            botonMenu.setAttribute('aria-expanded', 'true');
+        }
+
     });
+
 }
+
+formularioRegistro.addEventListener('submit', (evento) => {
+
+
+    const nombre = inputNombre.value.trim();
+    const email = inputEmailRegistro.value.trim();
+    const rol = selectPerfil.value;
+    const password = inputPasswordRegistro.value.trim();
+    const aceptoTerminos = checkTerminos ? checkTerminos.checked : false;
+
+    if (nombre === '' || password === '' || email === '') {
+        alert('Por favor, llena todos los campos');
+    }
+
+    if (!aceptoTerminos) {
+        alert('Por favor, acepta los terminos');
+    }
+
+    const usuarioRegistrado = {
+        nombre: nombre,
+        email: email,
+        rol: rol,
+        password: password
+    };
+
+    localStorage.setItem('usuarioRegistrado', JSON.stringify(usuarioRegistrado));
+    alert('¡Cuenta creada exitosamente! Ahora puedes iniciar sesión.');
+
+    window.location.href = 'login.html';
+
+
+});
