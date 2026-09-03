cff563340e786a435dc1c65c18c7a26e6c1228c2
JxberV <jabervargas1726@gmail.com>
2026-09-03 00:00:36 -0500

    wip: style frontend

---
+++ 03-Development/Src/Frontend/Scripts/app.js
@@ -0,0 +1,9 @@
+const menuToggle = document.querySelector('.menu-toggle');
+const mainNav = document.querySelector('.main-nav');
+
+if (menuToggle && mainNav) {
+    menuToggle.addEventListener('click', () => {
+        const isOpen = mainNav.classList.toggle('open');
+        menuToggle.setAttribute('aria-expanded', String(isOpen));
+    });
+}
